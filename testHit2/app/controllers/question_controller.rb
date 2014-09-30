840681d0a1ff95d1f07e6cd34c477e6f315af006
William Huang <whuang6@stanford.edu>
2014-09-29 20:42:10 -0700

    commit pg

---
+++ testHit2/app/controllers/question_controller.rb
@@ -0,0 +1,4 @@
+class QuestionController < ApplicationController
+	def pokemonOrDigimon
+	end
+end
---
+++ testHit2/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestHit2::Application.config.secret_key_base = '9e7806d89aa21e7ded1fdfffe9224a7f6d3921dc16b969521b9adeef30dfbe07f6fbba894212aa2433ce125a5d4448c34a6278db87540a115a5281593d8542d4'
